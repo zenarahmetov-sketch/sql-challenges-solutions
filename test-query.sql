75e78843af300cce35ee3d9768657cc7cf2117a7
zenarahmetov-sketch <zenarahmetov@gmail.com>
2026-03-05 18:52:00 +0300

    Update test-query.sql

--- test-query.sql
+++ test-query.sql
@@ -1,3 +1,13 @@
--- Title: Test Query
--- Difficulty: Easy
-SELECT * FROM users WHERE city = 'Kuala Lumpur';
+-- Title: Top 5 Highest Paid Employees
+-- Difficulty: Medium
+-- Category: Joins and Aggregation
+
+SELECT 
+    e.Name, 
+    d.Name AS Department, 
+    e.Salary
+FROM Employees e
+JOIN Departments d ON e.DepartmentID = d.ID
+WHERE e.Salary > 5000
+ORDER BY e.Salary DESC
+LIMIT 5;
